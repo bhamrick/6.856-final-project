2f776d168add2d6ac7eb4ee0645937ef31ae5016
tjhance <tjhance7@gmail.com>
2011-05-02 15:32:54 -0400

    added point functions to shared.h

--- shared.cpp
+++ shared.cpp
@@ -1,8 +1,11 @@
 #include <cmath>
 #include <cstdlib>
+#include <cassert>
 #include <shared.h>
 
-void random_direction(int n, real *vec) {
+void random_direction(int n, point *p) {
+	p->n = n;
+	p->x = new real[n];
 	for(int i = 0; i < n; i += 2) {
 		real x, y, s;
 		do {
@@ -11,9 +14,26 @@ void random_direction(int n, real *vec) {
 			s = x*x + y*y;
 		} while(s >= (real)1.0);
 		s = sqrt(-log(s) / s);
-		vec[i] = x * s;
+		p->x[i] = x * s;
 		if(i + 1 < n)
-			vec[i+1] = y * s;
+			p->x[i+1] = y * s;
 	}
 }
 
+point::point(int n) {
+	this->n = n;
+	this->x = new real[n];
+}
+
+point & point::operator+=(point other) {
+	assert(this->n == b.n);
+	for(int i = 0; i < n; i++)
+		this->x[i] += other.x[i];
+	return *this;
+}
+
+void move_along_line(point a, point dir, real s, point* result) {
+	assert(a.n == b.n && a.n == result->n);
+	for(int i = 0; i < n; i++)
+		result->x[i] = a.x[i] + dir.x[i] * s;
+}
--- shared.h
+++ shared.h
@@ -6,8 +6,12 @@ typedef double real;
 struct point {
 	int n;
 	real* x;
+	point & operator+=(point other);
+	point(int n);
 };
 
 void random_direction(int n, point* p);
+
+void move_along_line(point a, point dir, real s, point* result);
 
 #endif
