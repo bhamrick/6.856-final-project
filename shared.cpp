71561b33708d31096d46d354af60d3c6dd464cce
tjhance <tjhance7@gmail.com>
2011-05-02 14:57:45 -0400

    shared.cpp

---
+++ shared.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <crand>
+
+typedef real double;
+
+void random_direction(int n, real *vec) {
+	for(int i = 0; i < n; i += 2) {
+		real x, y, s;
+		do {
+			x = (real)(rand() - RAND_MAX/2) / (real)(RAND_MAX/2);
+			y = (real)(rand() - RAND_MAX/2) / (real)(RAND_MAX/2);
+			s = x*x + y*y;
+		} while(s >= (real)1.0);
+		s = sqrt(-log(s) / s);
+		vec[i] = x * s;
+		if(i + 1 < n)
+			vec[i+1] = y * s;
+	}
+}
+
+void sample(int n, int T, real *start, bool (*separation_oracle)(real *)) {
+
+}
