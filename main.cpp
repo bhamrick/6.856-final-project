8a666da3645380012355ac49001ce11b53d8d8c0
bhamrick <brian.c.hamrick@gmail.com>
2011-05-02 15:43:10 -0400

    Modified makefile and some others

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include<sample.h>
+#include<cstdio>
+#include<cstdlib>
+
+int main() {
+	
+}
--- sample.h
+++ sample.h
@@ -1,6 +1,8 @@
 #ifndef SAMPLE_H
 #define SAMPLE_H
 
+#include<shared.h>
 
+point random_sample(int n, point p, bool (*inside)(const point));
 
 #endif
